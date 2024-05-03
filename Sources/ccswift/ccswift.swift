c365c6b624e844919488f31c07ca048c6c056deb
arafetksiksii <80582941+arafetksiksii@users.noreply.github.com>
2024-05-03 23:19:52 +0100

    Update ccswift.swift

--- Sources/ccswift/ccswift.swift
+++ Sources/ccswift/ccswift.swift
@@ -37,35 +37,35 @@ class JSONUtility {
 
 class SocketObject: ObservableObject {
     static var shared = SocketObject()
-    let ipAddress = "http://192.168.78.249:9090"
+    let ipAddress = "http://172.18.23.27:9090"
     let service = Service()
     var manager: SocketManager!
     var socket: SocketIOClient!
     var status: String!
-    
+
     init() {
-        self.manager = SocketManager(socketURL: URL(string:ipAddress)!, config: [.log(false), .compress])
+        self.manager = SocketManager(socketURL: URL(string: ipAddress)!, config: [.log(false), .compress])
         self.socket = self.manager.defaultSocket
-        
-        socket.on(clientEvent: .connect) { (data,ack) in
-                print("Socket connected")
+
+        socket.on(clientEvent: .connect) { (data, ack) in
+            print("Socket connected")
         }
-        socket.on(clientEvent: .disconnect) { (data,ack) in
-                print("Socket disconnected")
+        socket.on(clientEvent: .disconnect) { (data, ack) in
+            print("Socket disconnected")
         }
-        socket.onAny{ (event) in
+        socket.onAny { (event) in
             let responseObj = JSONUtility.getJson(objects: event.items!)
             print("----------- %@\n\n", event.event, event.items as Any)
             print("----------- %@\n items %@", event.event, responseObj as Any)
         }
         socket.on("UpdateSocket") { (object, ack) in
             print("Socket response UpdateSocket : %@", object)
-            
+
             if (object.count > 0) {
                 if let responseObj = object[0] as? NSDictionary {
                     if responseObj.value(forKey: self.status) as? String ?? "" == "1" {
-                        print("succes status")
-                    }else{
+                        print("success status")
+                    } else {
                         print("fail status")
                     }
                 }
@@ -76,41 +76,35 @@ class SocketObject: ObservableObject {
         }
         socket.connect()
     }
+
     func emit(event: String, with items: NSArray) {
-    switch self.socket.status {
+        switch self.socket.status {
         case .connected:
             self.socket.emit(event, items)
+        case .connecting:
+            print("\n\n ---------- Connecting ... --------------/n/n", event)
+            self.socket.once(clientEvent: .connect) { (object, ack) in
+                self.socket.emit(event, items)
+                print("\n\n---------ConnectOnce-----\n\n", event)
+            }
+        case .disconnected:
+            print("\n\n ---------- Disconnected --------------/n/n", event)
+        default:
             break
-    case .connecting:
-        print("\n\n ---------- Connecting ... --------------/n/n", event)
-        self.socket.once(clientEvent: .connect) {(object, ack) in
-            self.socket.emit(event, items)
-            print("\n\n---------ConnectOnce-----\n\n", event)
-        }
-        break
-    case.disconnected:
-        print("\n\n ---------- Disconnected --------------/n/n", event)
-        break
-    default:
-        break
         }
     }
-    func updateSocket(){
-        
-    }
+
     func joinConversation(conversationId: String) {
         switch self.socket.status {
         case .connected:
             socket.emit("join_conversation", conversationId)
         case .connecting:
-            // If the socket is still connecting, wait for it to connect and then emit the event
             print("Socket is still connecting...")
             self.socket.once(clientEvent: .connect) { [weak self] _, _ in
                 guard let self = self else { return }
                 self.socket.emit("join_conversation", conversationId)
             }
         case .disconnected:
-            // If the socket is disconnected, handle the situation accordingly
             print("Socket is disconnected.")
         default:
             break
@@ -123,12 +117,10 @@ class SocketObject: ObservableObject {
             "conversation": conversationId,
             "content": message,
             "type": type
-
         ]
         socket.emit("new_message_\(conversationId)", messageData)
     }
 
-    
     func listenForMessages(conversationId: String, completion: @escaping ([MessagesStructure]) -> Void) {
         // Listen for new messages
         socket.on("new_message_\(conversationId)") { [weak self] (data, ack) in
@@ -196,37 +188,7 @@ class SocketObject: ObservableObject {
     }
 
 
-
-
-    private func updateMessages(with newMessage: MessagesStructure, conversationId: String, completion: @escaping ([MessagesStructure]) -> Void) {
-        // Fetch existing messages for the conversation
-        service.fetchMessages(conversationId: conversationId) { [weak self] json, error in
-            guard let self = self else { return }
-            if let error = error {
-                print("Error fetching messages: \(error)")
-                return
-            }
-            var updatedMessages: [MessagesStructure] = []
-            if let json = json, let messagesData = json["messages"] as? [[String: Any]] {
-                updatedMessages = messagesData.compactMap { messageData in
-                    MessagesStructure(
-                        id: messageData["_id"] as? String ?? "",
-                        sender: messageData["sender"] as? String ?? "",
-                        content: messageData["content"] as? String ?? "",
-                        timestamp: messageData["timestamp"] as? String ?? "",
-                        type: messageData["type"] as? String ?? "",
-                        emoji: (messageData["emojis"] as? [String])?.first ?? ""
-                    )
-                }
-            }
-            updatedMessages.append(newMessage) // Append the new message
-            print("Updated messages:", updatedMessages)
-            completion(updatedMessages)
-        }
-    }
-
 }
-
 
 
 
@@ -417,7 +379,7 @@ struct IncomingDoubleLineMessage: View {
 
 
 
-public struct MessengerView: View {
+struct MessengerView: View {
     let service = Service()
     let senderName: String
     let conversationId: String
@@ -425,7 +387,7 @@ public struct MessengerView: View {
     @Environment(\.presentationMode) var presentationMode
     @StateObject private var socketObject = SocketObject.shared
 
-    public var body: some View {
+    var body: some View {
         VStack {
             HStack {
                 Button(action: {
@@ -433,6 +395,9 @@ public struct MessengerView: View {
                 }) {
                     Image(systemName: "arrow.left")
                         .padding(.leading, -10)
+                }.onDisappear {
+                    // Disconnect from socket when leaving the view
+                    socketObject.socket.disconnect()
                 }
                 Image(senderName)
                     .resizable()
@@ -442,8 +407,6 @@ public struct MessengerView: View {
                     .overlay(Circle().stroke(Color.white, lineWidth: 2))
                     .shadow(radius: 3)
                 VStack(alignment: .leading) {
-                    
-
                     Text(senderName)
                         .font(.title)
                         .foregroundColor(.black)
@@ -455,48 +418,56 @@ public struct MessengerView: View {
                 Text("Online")
                     .font(.caption)
                     .foregroundColor(.gray)
-
                 Spacer()
-                
-            /*  NavigationLink(destination: VideoCallView(currentUser: service.currentUser, conversationId: conversationId)) {
+     
                     Image(systemName: "video.fill")
                         .font(.title)
                         .foregroundColor(.blue)
-                }*/
+                
                 Image(systemName: "phone.fill")
                     .font(.title)
                     .foregroundColor(.blue)
             }
             .padding()
             
-            ScrollView {
-                VStack(spacing: 1) {
-                    ForEach(messages.indices, id: \.self) { index in
-                        HStack {
-                            if messages[index].sender == service.currentUser {
-                                OutgoingDoubleLineMessage(message: messages[index]) // Unwrap the binding
-                            } else {
-                                IncomingDoubleLineMessage(message: messages[index]) // Unwrap the binding
-                            }
-                            if messages[index].sender != service.currentUser {
-                                EmojiButton(conversationId: conversationId, messageId: messages[index].id, service: service) // Pass necessary parameters
-                                    .foregroundColor(.gray)
+            GeometryReader { geometry in
+                ScrollViewReader { scrollView in
+                    ScrollView {
+                        VStack(spacing: 1) {
+                            ForEach(messages.indices, id: \.self) { index in
+                                HStack {
+                                    if messages[index].sender == service.currentUser {
+                                        OutgoingDoubleLineMessage(message: messages[index])
+                                    } else {
+                                        IncomingDoubleLineMessage(message: messages[index])
+                                    }
+                                    if messages[index].sender != service.currentUser {
+                                        EmojiButton(conversationId: conversationId, messageId: messages[index].id, service: service)
+                                            .foregroundColor(.gray)
+                                    }
+                                }
+                                .padding()
+                                .id(index)
                             }
                         }
-                        .padding()
+                        .onChange(of: messages.count) { _ in
+                            if messages.count > 0 {
+                                withAnimation {
+                                    scrollView.scrollTo(messages.count - 1, anchor: .bottom)
+                                }
+                            }
+                        }
                     }
                 }
             }
-
             
             ComposeArea(conversationId: conversationId, currentUserId: service.currentUser)
         }
         .padding(.bottom)
         .navigationBarBackButtonHidden(true)
-        
         .onAppear {
             onAppear()
-          }
+        }
         .onDisappear{
             onDisappear()
         }
@@ -531,19 +502,18 @@ public struct MessengerView: View {
         socketObject.joinConversation(conversationId: conversationId)
     }
     
-    // Remove the listener when the view disappears
     func onDisappear() {
         socketObject.socket.off("new_message_\(conversationId)")
     }
     
     func listenForMessages() {
         socketObject.listenForMessages(conversationId: conversationId) { newMessages in
-            // Update the messages array with the newly received messages
             self.messages = newMessages
         }
     }
- }
- 
+}
+
+
 
 
 // Emoji button view
@@ -1003,7 +973,7 @@ struct ConversationDetailView: View {
 }
 
 class Service {
-    let ipAddress = "192.168.78.249:9090"
+    let ipAddress = "172.18.23.27:9090"
     let conversationId = "10.0.2.2"
     let currentUser = "participant2"
     func fetchMessages(conversationId: String, completion: @escaping ([String: Any]?, Error?) -> Void) {
